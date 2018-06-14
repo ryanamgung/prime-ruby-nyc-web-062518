b29a11d4df785e96a47e46d815e9ff6630cd76a4
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 18:47:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,4 @@
 # Add  code here!
 def prime?(integer)
-  var = true
-  if (integer.even? && integer > 2) || integer <= 1
-    false
-  elsif integer.odd?
-    if integer % 3 == 0 || integer % 5 == 0
-      false
-    end
-  end
   
-  return var
 end
-
-  true
