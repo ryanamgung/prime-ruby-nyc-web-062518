020424d835428aa36e1e37f61d477dcae9b31214
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 18:33:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,4 @@
 # Add  code here!
 def prime?(integer)
-  if integer == 2 || integer == 3 || integer == -2 || integer == -3
-    true
-  elsif integer % 2 == 0 || integer % 3 == 0 || integer % 5 == 0
-    false
-  else
-    true
+  if integer.iseven?
 end
