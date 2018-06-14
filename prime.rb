54e1898d4cb1cc27b07d7ef574e9facabe88a4fd
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 19:00:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,6 +5,7 @@ def prime?(integer)
   elsif integer == 2 || integer == 3
     true
   else 
+    #we iterate over the range from 2 to the number - 1, and if there is no solution to integer % divisor == 0 then we have a prime number
     (2..integer-1).none? do |divisor|
       integer % divisor == 0
     end
