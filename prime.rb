b0f9355320a1ac86f5d2fccce2e788b546a2ebd2
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 18:32:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+def prime?(integer)
+  if integer == 2 || integer == 3 || integer == -2 || integer == -3
+    true
+  elsif integer % 2 == 0 || integer % 3 == 0 || integer % 5 == 0
+    false
+  else
+    true
+end
